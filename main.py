fd8426260ab7eb969e464461cdb9b7bfc3044476
Bishnuksth057 <bishnukumarshrestha255044@gmail.com>
2025-07-05 00:30:27 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "7734449850:AAERA_47O3yoNhs8sT4uOhRILIaKEDmoSD0"  # Replace this with your actual bot token
+TOKEN = ("7734449850:AAERA_47O3yoNhs8sT4uOhRILIaKEDmoSD0") # Replace this with your actual bot token
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🎉 Spin and Win Bot is Live!")
